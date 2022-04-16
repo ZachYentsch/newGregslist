002f261900fbdae68d9deba6fc3d7df5a32a9e20
ZachYentsch <z6chy@icloud.com>
2022-04-15 18:05:29 -0600

    games Service client done

--- newGregslist.client/src/services/GamesService.js
+++ newGregslist.client/src/services/GamesService.js
@@ -6,27 +6,37 @@ class GamesService {
     async getAllGames() {
         const res = await api.get('api/games')
         logger.log(res.data)
-        AppState.games
+        AppState.games = res.data
     }
 
     async getGameById(id) {
-
+        const res = await api.get('api/games/' + id)
+        logger.log(res.data)
+        AppState.activeGame = res.data
     }
 
     async getMyGames() {
-
+        const res = await api.get('account/games')
+        logger.log(res.data)
+        AppState.myGames = res.data
     }
 
-    async createGame() {
-
+    async createGame(newGame) {
+        const res = await api.post('api/games', newGame)
+        logger.log(res.data)
+        return res.data
     }
 
-    async editGame() {
-
+    async editGame(updatedGame, id) {
+        const res = await api.put('api/games/' + id, updatedGame)
+        logger.log(res.data)
+        AppState.activeGame = res.data
     }
 
-    async removeGame() {
-
+    async removeGame(id) {
+        const res = await api.delete('api/games/' + id)
+        logger.log('Removed Game', res.data)
+        AppState.games = AppState.games.filter(g => g.id != g.id)
     }
 }
 
